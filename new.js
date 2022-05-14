5a8f90b4488645784247f3c75fac0f008f87e0bd
Shodydosh <skyzed0@gmail.com>
2022-05-14 23:18:02 +0700

    Update new.js
    fixing adding function

--- new.js
+++ new.js
@@ -14,35 +14,40 @@ function addNewTask(event){
     event.preventDefault();
     // Todo div
 
-    const todoDiv = document.createElement("div");
-    todoDiv.classList.add("todo");
     
-    // create LI
-    const newTodo = document.createElement("div");
-    newTodo.innerText = todoInput.value;
-    newTodo.classList.add('todo-item');
-    todoDiv.appendChild(newTodo);
+    if(todoInput.value === '') alert("You must write something!!");
+    else{
+        const todoDiv = document.createElement("div");
+        todoDiv.classList.add("todo");
+            // create LI
+        const newTodo = document.createElement("div");
+        newTodo.innerText = todoInput.value;
+        todoInput.value = '';
+        newTodo.classList.add('todo-item');
+        todoDiv.appendChild(newTodo);
 
-    const buttons = document.createElement("div");
-    buttons.classList.add("interactButtons");
+        const buttons = document.createElement("div");
+        buttons.classList.add("interactButtons");
 
-    // check mark button
-    const completedButton = document.createElement('button');
-    completedButton.innerHTML = '<i class="fa fa-check"></i>';
-    completedButton.classList.add("complete-btn");
-    buttons.appendChild(completedButton);
-    // check edit button
-    const editButton = document.createElement('button');
-    editButton.innerHTML = '<i class="fa fa-pen"></i>';
-    editButton.classList.add("edit-btn");
-    buttons.appendChild(editButton);
-    // check trash button
-    const trashButton = document.createElement('button');
-    trashButton.innerHTML = '<i class="fa fa-xmark"></i>';
-    trashButton.classList.add("delete-btn");
-    buttons.appendChild(trashButton);
+        // check mark button
+        const completedButton = document.createElement('button');
+        completedButton.innerHTML = '<i class="fa fa-check"></i>';
+        completedButton.classList.add("complete-btn");
+        buttons.appendChild(completedButton);
+        // check edit button
+        const editButton = document.createElement('button');
+        editButton.innerHTML = '<i class="fa fa-pen"></i>';
+        editButton.classList.add("edit-btn");
+        buttons.appendChild(editButton);
+        // check trash button
+        const trashButton = document.createElement('button');
+        trashButton.innerHTML = '<i class="fa fa-xmark"></i>';
+        trashButton.classList.add("delete-btn");
+        buttons.appendChild(trashButton);
 
-    todoDiv.appendChild(buttons);
-    // append to list 
-    todoList.appendChild(todoDiv);
+        todoDiv.appendChild(buttons);
+        // append to list 
+        todoList.appendChild(todoDiv);
+    }
+    
 }
